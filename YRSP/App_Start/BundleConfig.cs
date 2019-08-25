5cd656360465ec1b0ca6e29b7e9ad2a1220fc593
Pan Qi <qpan0003@student.monash.edu>
2019-08-26 07:39:56 +1000

    Adjustment

--- YRSP/App_Start/BundleConfig.cs
+++ YRSP/App_Start/BundleConfig.cs
@@ -8,10 +8,10 @@ namespace YRSP
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery/*.min.js",
-                        "~/Scripts/jquery/jquery-2.1.4.min.js"
-                        ));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery/*.min.js",
+            //            "~/Scripts/jquery/jquery-2.1.4.min.js"
+            //            ));
 
             //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
             //            "~/Scripts/jquery.validate*"));
@@ -24,19 +24,34 @@ namespace YRSP
             //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
             //          "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new ScriptBundle("~/Bundles/js").Include(
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
                 "~/Scripts/bootstrap.js",
                 "~/Scripts/classie.js",
                 "~/Scripts/demo1.js",
                 "~/Scripts/easing.js",
-                "~/Scripts/jquery.flipster.js",
-                "~/Scripts/jquery.pogo-slider.min.js",
-                "~/Scripts/lsb.min.js",
-                "~/Scripts/main.js",
                 "~/Scripts/modernizr.js",
                 "~/Scripts/move-top.js",
-                "~/Scripts/numscroller-1.0.js",
+                "~/Scripts/numscroller-1.0.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/flipster").Include(
+                "~/Scripts/jquery.flipster.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/owlcarousel").Include(
                 "~/Scripts/owl.carousel.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/lsb").Include(
+                "~/Scripts/lsb.min.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/pogo").Include(
+                "~/Scripts/jquery.pogo-slider.min.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/main").Include(
+                "~/Scripts/main.js"
                 ));
 
             bundles.Add(new ScriptBundle("~/Bundles/population").Include(
--- YRSP/Controllers/HomeController.cs
+++ YRSP/Controllers/HomeController.cs
@@ -20,11 +20,11 @@ namespace YRSP.Controllers
             return View();
         }
 
-        //public ActionResult Contact()
-        //{
-        //    ViewBag.Message = "Your contact page.";
+        public ActionResult Education()
+        {
+            //ViewBag.Message = "Your contact page.";
 
-        //    return View();
-        //}
+            return View();
+        }
     }
 }
