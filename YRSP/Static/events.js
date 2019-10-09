37e9fe99b6ac09f24a64c32457518ee6da405b69
Pan Qi <qipanlml@gmail.com>
2019-10-10 00:22:46 +1100

    Events filter

--- YRSP/Static/events.js
+++ YRSP/Static/events.js
@@ -7,7 +7,8 @@
             description: 'Are you wanting to wanting to improve your ability to work or engage with culturally diverse individuals and families? The Brotherhood of St Laurence is offering this interactive cross-cultural responsiveness training, facilitated by practitioners with first-hand casework experience, to provide practical strategies. We’ll tailor the material to the specific needs of the group, but it will largely be aimed at those working with families. BYO lunch and snacks.\n9.30am-4.30pm, Friday 15th November, 2019.',
             img: 'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F64404773%2F233052538577%2F1%2Foriginal.20190519-194723?h=200&w=450&auto=compress&rect=303%2C64%2C670%2C335&s=168424b93bc74a5b05f59f4169faad48',
             location: 'Coburg',
-            category: 'Community & Culture'
+            category: 'Community & Culture',
+            is_free: false
         },
         {
             title: 'Fundraiser for Utopia Refugee and Asylum Seeker Health Clinic',
@@ -17,6 +18,7 @@
             img: 'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F73836411%2F4363508232%2F1%2Foriginal.20190922-092608?h=200&w=450&auto=compress&rect=160%2C0%2C632%2C316&s=941cabd38aed401ee4522257ee2835b6',
             location: 'Yarraville',
             category: 'Film, Media & Entertainment',
+            is_free: false
         },
         {
             title: `Community & Union Organising with Workers and Communities of Refugee & Migrant Backgrounds - Fitzroy`,
@@ -26,6 +28,7 @@
             img: `https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F72017999%2F233052538577%2F1%2Foriginal.20190909-011059?h=200&w=450&auto=compress&rect=0%2C105%2C1280%2C640&s=e2e654f624c0bd496c523506450af8d8`,
             location: `Fitzroy`,
             category: `Community & Culture`,
+            is_free: false
         },
         {
             title: `Your rights at work: Migrant Workers Centre`,
@@ -35,6 +38,7 @@
             img: `https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F61801678%2F100525113303%2F1%2Foriginal.20190507-020611?h=200&w=450&auto=compress&rect=0%2C0%2C2160%2C1080&s=2d17fb941d0fc96e0e70455e61ef4d6d`,
             location: `CBD`,
             category: `Government & Politics`,
+            is_free: true
         },
         {
             title: `Working with Families of Refugee & Migrant Backgrounds Training - Dandenong`,
@@ -44,6 +48,7 @@
             img: `https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F66932427%2F233052538577%2F1%2Foriginal.20190519-194723?h=200&w=450&auto=compress&rect=303%2C64%2C670%2C335&s=911b0ad687c307a33d607dc3da328055`,
             location: `Dandenong`,
             category: `Community & Culture`,
+            is_free: false
         },
     ]
 }
@@ -57,4 +62,5 @@
 //    img: ``,
 //    location: ``,
 //    category: ``,
+//    is_free: true
 //},
