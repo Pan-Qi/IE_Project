5cae7461b0e39ddfbbff604c39f44db4cfc3f245
Pan Qi <qipanlml@gmail.com>
2019-10-09 22:30:57 +1100

    Front page introduction block

--- YRSP/Scripts/Visualisation/schoolVisualization.js
+++ YRSP/Scripts/Visualisation/schoolVisualization.js
@@ -37,7 +37,7 @@ function drawChart() {
       pieHole: 0.4,
       width: '100%',
       height: '500px',
-      chartArea: {left:10,top:5}, // paddings for chartArea
+        chartArea: { left: 10, top: 5 }// paddings for chartArea
         //colors: ['orange','#00cc33', 'pink', 'yellow','lightblue']
     };
     // draw the chart
